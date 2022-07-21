0f054b1a934aecd8ffb47a90b2a99ab071d2dfa8
Benji Mba <benjimba@Benjis-MacBook-Pro.local>
2022-07-21 15:29:14 +0000

    Updated accessibe script

--- resources/views/script.blade.php
+++ resources/views/script.blade.php
@@ -1,3 +1,40 @@
-@if(Astrogoat\Accessibe\Settings\AccessibeSettings::isEnabled() && $url = settings('Astrogoat\\Accessibe\\Settings\\AccessibeSettings', 'url'))
-    <script>(function(){ var s = document.createElement('script'); var h = document.querySelector('head') || document.body; s.src = "{{ $url }}"; s.async = true; s.onload = function(){ acsbJS.init({ statementLink : '', footerHtml : '', hideMobile : false, hideTrigger : false, disableBgProcess : false, language : 'en', position : 'right', leadColor : '#146FF8', triggerColor : '#146FF8', triggerRadius : '50%', triggerPositionX : 'right', triggerPositionY : 'bottom', triggerIcon : 'people', triggerSize : 'bottom', triggerOffsetX : 20, triggerOffsetY : 20, mobile : { triggerSize : 'small', triggerPositionX : 'right', triggerPositionY : 'bottom', triggerOffsetX : 20, triggerOffsetY : 20, triggerRadius : '20' } }); }; h.appendChild(s); })();</script>
+@if (Astrogoat\Accessibe\Settings\AccessibeSettings::isEnabled() &&
+    ($url = settings('Astrogoat\\Accessibe\\Settings\\AccessibeSettings', 'url')))
+    <script>
+        (function() {
+            var s = document.createElement('script');
+            var h = document.querySelector('head') || document.body;
+            s.src = "{{ $url }}";
+            s.async = true;
+            s.onload = function() {
+                acsbJS.init({
+                    statementLink: '',
+                    footerHtml: '',
+                    hideMobile: false,
+                    hideTrigger: false,
+                    disableBgProcess: false,
+                    language: 'en',
+                    position: 'left',
+                    leadColor: '#000000',
+                    triggerColor: '#000000',
+                    triggerRadius: '50%',
+                    triggerPositionX: 'left',
+                    triggerPositionY: 'bottom',
+                    triggerIcon: 'people',
+                    triggerSize: 'small',
+                    triggerOffsetX: 20,
+                    triggerOffsetY: 20,
+                    mobile: {
+                        triggerSize: 'small',
+                        triggerPositionX: 'left',
+                        triggerPositionY: 'bottom',
+                        triggerOffsetX: 10,
+                        triggerOffsetY: 10,
+                        triggerRadius: '50%'
+                    }
+                });
+            }
+            h.appendChild(s);
+        })();
+    </script>
 @endif
